91af25f42fe8bf586c2f7cd3fa6901ec2d579776
Mitch Jenkins <mitchellkjenkins@hotmail.com>
2014-12-29 20:28:48 -0500

    added pages and css links for pages

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,16 @@
+class PagesController < ApplicationController
+  def home
+  end
+
+  def news
+  end
+
+  def music
+  end
+
+  def video
+  end
+
+  def contact
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  root "pages#home"
+  get "news" => "pages#news"
+  get "music" => "pages#music"
+  get "video" => "pages#video"
+  get "contact" => "pages#contact"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
